14f0548519e8d6232cb141cc3ce353fa791fa993
Mila Bozorova <n0ragami@mail.ru>
2024-04-15 21:32:14 +0500

    Результаты урока и тест на переход в Сообщества

--- Seleniumtests_bozorova/SeleniumTestForPractice.cs
+++ Seleniumtests_bozorova/SeleniumTestForPractice.cs
@@ -1,11 +1,16 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 
 namespace Seleniumtest_bozorova;
 
 public class SeleniumTestForPractice
 {
+
+    public ChromeDriver driver;
+    
     [Test]
     public void Authorization()
     {
@@ -13,10 +18,19 @@ public class SeleniumTestForPractice
         options.AddArguments("--no-sandbox", "--start-maximized", "--disable-extensions");
         
         // -звйти в хром ( с помощью вебдрайвера)
-        var driver = new ChromeDriver(options);
+        driver = new ChromeDriver(options);
+        
+        //Неявное ожидание
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+        
+        //ожидания появления страницы
         
         // -перейти по урлу https://staff-testing.testkontur.ru
         driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru");
+
+        //явные ожидания
+        var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
+        wait.Until(ExpectedConditions.ElementIsVisible(By.Id("Username")));
         
        
         //-ввести логин и пароль
@@ -26,20 +40,43 @@ public class SeleniumTestForPractice
         var password = driver.FindElement(By.Name("Password"));
         password.SendKeys("1q2w3e4r%T");
         
-        Thread.Sleep(3000);
-
+        
         // -нажать на кнопку "войти"
         var enter = driver.FindElement(By.Name("button"));
         enter.Click();
         
-        Thread.Sleep(3000);
+        //
+        
+        // Thread.Sleep(3000);
         
         // - проверяем что находимся на нужной странице
-        var currentUrl = driver.Url;
-        Assert.That(currentUrl == "https://staff-testing.testkontur.ru/news");
+        // var currentUrl = driver.Url;
+        // Assert.That(currentUrl == "https://staff-testing.testkontur.ru/news", $"Фактический URL {currentUrl}");
         
-        
+        Assert.That(driver.FindElement(By.CssSelector("h1[data-tid='Title']")) is not null);
+
         //- закрываем браузер и убиваем процесс драйвера
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
         driver.Quit();
+    }
+
+    [Test]
+    public void TestMenu()
+    {
+        var options = new ChromeOptions();
+        options.AddArguments("--no-sandbox", "--start-maximized", "--disable-extensions");
+        driver = new ChromeDriver(options);
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+        driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru");
+        driver.FindElement(By.Id("Username")).SendKeys("user");
+        driver.FindElement(By.Name("Password")).SendKeys("1q2w3e4r%T");
+        driver.FindElement(By.Name("button")).Click();
+        Assert.That(driver.FindElement(By.CssSelector("h1[data-tid='Title']")) is not null);
+        driver.FindElement(By.XPath("//span[contains(text(), 'Сообщества')]")).Click();
+        Assert.That(driver.FindElement(By.XPath("//h1[contains(text(), 'Сообщества')]")) is not null);
     }
 }
